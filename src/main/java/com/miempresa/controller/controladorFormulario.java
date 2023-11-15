80bfb0d7e767acbcc472c557777835389b75f1ff
josearcos2023 <jose.arcos@tecsup.edu.pe>
2023-11-15 17:14:11 -0500

    lab12 commit

--- src/main/java/com/miempresa/controller/controladorFormulario.java
+++ src/main/java/com/miempresa/controller/controladorFormulario.java
@@ -1,9 +1,14 @@
 package com.miempresa.controller;
 
+import com.miempresa.DTO.formularioDTO;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class controladorFormulario {
 
     @GetMapping("/formularioRegistro")
-    public String envioFormulario
+    public String envioFormulario(Model model){
+        model.addAttribute("form", new formularioDTO());
+        return "formulario";
+    }
 }
